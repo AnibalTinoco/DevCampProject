7ff186c5d9a615d9c9500e63c1da53fb1fb36160
Jordan Hudgens <jordan@devcamp.com>
2017-07-05 15:27:58 +0000

    Implemented partials with local data for portfolio from and nav bars

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,18 +1,19 @@
 class PortfoliosController < ApplicationController
   layout 'portfolio'
+  
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
-  
+
   def new
-  @portfolio_item = Portfolio.new
-  3.times { @portfolio_item.technologies.build }
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
-  
+
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
@@ -24,40 +25,41 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
-    def edit
-      @portfolio_item = Portfolio.find(params[:id])
-    end
-    
-     def update
-       @portfolio_item = Portfolio.find(params[:id])
-       
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
   end
-  
+
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
-  
-    def destroy
-      # Perform the lookup
-      @portfolio_item = Portfolio.find(params[:id])
-      
-      # Destroy/delete the record
-      @portfolio_item.destroy
-    
-      # Redirect
+
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
     respond_to do |format|
-      format.html { redirect_to portfolio_url, notice: 'Record was removed.' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-  
+
   private
 
   def portfolio_params
@@ -67,5 +69,5 @@ class PortfoliosController < ApplicationController
                                       technologies_attributes: [:name]
                                      )
   end
-    
+
 end
